19fbc81bc9f45ff6afbc28a8617a744a2fffcbab
codewithjo3 <sometimewithjoel@gmail.com>
2021-05-26 16:44:31 +0530

    savedd

--- app/Http/Controllers/registercontroller.php
+++ app/Http/Controllers/registercontroller.php
@@ -276,6 +276,7 @@ class registercontroller extends Controller
             $login->Usertype="Customer";
     
             $login->save();
+            $h=registermodel::all();
             dd($signup);
             
             if($signup && $login)
